b18f5a768f7e8d6fc6a3791fe27b4431dca8e441
Volkan ÇİÇEK <v.cicek2018@gtu.edu.tr>
2020-08-05 17:01:49 +0300

    firebase update

--- dotnetwithReact/ClientApp/src/App.js
+++ dotnetwithReact/ClientApp/src/App.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import './custom.css';
+import fire from './fire';
+class App extends Component {
 
-import './custom.css'
+    state = {
+        text: ""
+    }
 
-export default class App extends Component {
-  static displayName = App.name;
+    handleText = e => {
+        this.setState({
+            text: e.target.value
+        })
+    }
+    handleSubmit = e => {
+        let messageRef = fire.database().ref('messages').orderByKey().limitToLast(100);
+        fire.database().ref('messages').push(this.state.text);
+        this.setState({
+            text: ""
+        })
+    }
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <div className="App-header">
+                <input type="text" onChange={this.handleText} id="inputText" />
+                <button onClick={this.handleSubmit}> Save </button>
+            </div>
+        );
+    }
 }
+
+export default App;
---
+++ dotnetwithReact/ClientApp/src/fire.js
@@ -0,0 +1,15 @@
+﻿import firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCU6539Ru0I4xJgfcW7hsVoWXa9FhBz55Y",
+    authDomain: "dotnetwithreact.firebaseapp.com",
+    databaseURL: "https://dotnetwithreact.firebaseio.com",
+    projectId: "dotnetwithreact",
+    storageBucket: "dotnetwithreact.appspot.com",
+    messagingSenderId: "877310800452",
+    appId: "1:877310800452:web:3785da3a98715c08adc211",
+    measurementId: "G-VB05G7NNS7"
+};
+
+var fire = firebase.initializeApp(firebaseConfig)
+export default fire;
